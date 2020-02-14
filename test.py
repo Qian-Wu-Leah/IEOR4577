6134f109936a757847ab3737567c5f0845c0af27
Qian-Wu-Leah <qwu0101@gmail.com>
2020-02-14 15:30:20 -0500

    no message

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ class TestMyModule(unittest.TestCase):
     def setUp(self):
         return
 
+    # @pytest.fixture
     def test_do_divide(self):
 
         fist_arg = 4
@@ -15,11 +16,15 @@ class TestMyModule(unittest.TestCase):
         expected_result = 2
         self.assertEqual(result, expected_result)
 
-    def test_do_divide_by_zero(self):
+#     # @pytest.fixture
+#     def test_do_divide_by_zero(self):
 
-        fist_arg = 4
-        second_arg = 0
+#         fist_arg = 4
+#         second_arg = 2
 
-        result = class_example.do_divide(fist_arg,second_arg)
-        expected_result = None
-        self.assertEqual(result, expected_result)
+#         result = class_example.do_divide(fist_arg,second_arg)
+#         expected_result = None
+#         self.assertEqual(result, expected_result)
+
+# # if __name__ =="main":
+#     # unittest.main()
